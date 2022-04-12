465081feeec3d988fd09cd55e76dcbbc4c4dc400
Vladimir Sokolovsky <vlad@nvidia.com>
2022-04-12 13:58:26 -0500

    Import DOCA telemetry container image
    Signed-off-by: Vladimir Sokolovsky <vlad@nvidia.com>

---
+++ src/import_doca_telemetry.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ${0%*/*}
+
+doca_telemetry=`/bin/ls doca_telemetry_service*arm64.tar`
+
+if [ ! -n $doca_telemetry ]; then
+	echo "DOCA telemetry container image was not found"
+	exit 1
+fi
+
+ex()
+{
+	eval "$@"
+	rc=$?
+
+	if [ $rc -ne 0 ]; then
+		echo "ERROR: Failed executing $@ RC: $rc"
+		exit $rc
+	fi
+}
+
+ex systemctl enable kubelet.service
+ex systemctl enable containerd.service
+
+ex systemctl start kubelet.service
+ex systemctl start containerd.service
+
+ex ctr --namespace k8s.io image import $doca_telemetry
